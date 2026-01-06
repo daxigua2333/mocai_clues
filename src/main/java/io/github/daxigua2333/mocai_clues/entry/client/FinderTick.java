2c0f6cbe59c72e4fcc09173dc608858ce7f4f88d
daxigua2333 <1318522478@qq.com>
2026-01-06 22:10:38 +0800

    alpha2.28 bug fix: mysterious dedicate server bug on Finder texture hook

--- src/main/java/io/github/daxigua2333/mocai_clues/entry/client/FinderTick.java
+++ src/main/java/io/github/daxigua2333/mocai_clues/entry/client/FinderTick.java
@@ -8,6 +8,7 @@ import io.github.daxigua2333.mocai_clues.data.client.api.ClientAccessor;
 import io.github.daxigua2333.mocai_clues.items.ModItemsRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
@@ -17,20 +18,23 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
+@OnlyIn(Dist.CLIENT)
 public final class FinderTick {
 
     private static boolean prevDoFound = false;
     // texture reflection
-    public static void registerTextureChange(){
+    @OnlyIn(Dist.CLIENT) public static void registerTextureChange(){
         ItemProperties.register(
                 ModItemsRegistry.CLUE_FINDER_ITEM.get(),
                 ResourceLocation.fromNamespaceAndPath(MoCaiClues.MODID, "found"),
                 (ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity, int id) -> {
-                    if (!(entity instanceof Player player)) return returnWithUpdate(false);
+                    if (!(entity instanceof LocalPlayer player)) return 0f;  // on multi-players this entity will be both LocalPlayer and RemotePlayer, thus cause unintended updates
                     if (player != Minecraft.getInstance().player) return returnWithUpdate(false);
 
                     // finder in hand
@@ -67,6 +71,7 @@ public final class FinderTick {
 
 //                    if (inHand && !data.isEmpty()) {
                     if (inHand && doFound) {
+//                        MoCaiClues.LOGGER.debug("{}", prevDoFound);
                         if (!prevDoFound) player.playSound(SoundEvents.EXPERIENCE_ORB_PICKUP, 1.0F, 1.0F);
                         return returnWithUpdate(true);
                     } else {
